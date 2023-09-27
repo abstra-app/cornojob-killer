0cf9d7ff6a421798c1e905b0c78100e0eebba5c3
Bruno Vieira Costa <bruno@abstra.app>
2023-09-27 19:48:20 -0300

    bold version

--- cornojob_killer/cli.py
+++ cornojob_killer/cli.py
@@ -3,20 +3,21 @@ import tempfile
 
 
 """
-If you're reading this, you're really paranoic with safety, right?
+If you're reading this, you're really paranoid about safety, right?
 
-Even though this is a joke, we take security very seriously.
+Although this is a joke, we take security extremely seriously. 
 Abstra is the safest and fastest way to automate your company's processes.
 
-We're not kidding.
+We're not kidding about that.
 
 Want to know more? Contact us at help@abstra.io
 """
 def main():
-    print("TODO: Funny message")
+    print("So you're a corno? Your secret is safe with us. ¯\\_(ツ)_/¯")
+    print("Welcome to cornojob-killer - the only weapon you'll ever need to destroy annoying, repetitive tasks.")
     print("For serious use cases, use the `abstra` CLI")
-    input()
-    print("Abstra will start in a temporary directory")
+    input("Ready to kill your corno jobs?")
+    print("Abstra will start in a temporary directory. Changes will not be saved.")
     print("For serious use cases, use the `abstra` CLI")
     print("Read the docs at docs.abstra.io")
     temp_dir = tempfile.mkdtemp()
