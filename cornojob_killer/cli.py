9c4a9da3fb253d619ef6b5912946a9e5d9b42d70
Bruno Vieira Costa <bruno@abstra.app>
2023-09-28 12:47:13 -0300

    linting

--- cornojob_killer/cli.py
+++ cornojob_killer/cli.py
@@ -1,6 +1,7 @@
 from abstra.cli import CLI
 import tempfile, time, requests, uuid, hashlib, threading
 
+
 def count():
     """
     Counting function to measure the number of times this lib was used.
@@ -9,9 +10,13 @@ def count():
     sha256_hash = hashlib.sha256()
     sha256_hash.update(str(uuid.getnode()).encode("utf-8"))
     hashed_string = sha256_hash.hexdigest()
-    requests.get("https://cornojob-killer.abstra.app/_hooks/count", headers={
-        "counter": hashed_string # We don't want to know who you are, we just want to know how many people are using this. :)
-    })
+    requests.get(
+        "https://cornojob-killer.abstra.app/_hooks/count",
+        headers={
+            "counter": hashed_string  # We don't want to know who you are, we just want to know how many people are using this. :)
+        },
+    )
+
 
 """
 If you're reading this, you're really paranoid about safety, right?
@@ -23,9 +28,13 @@ We're not kidding about that.
 
 Want to know more? Contact us at help@abstra.io
 """
+
+
 def main():
     threading.Thread(target=count).start()
-    print("Welcome to cornojob-killer - the only weapon you'll ever need to destroy annoying, repetitive tasks.")
+    print(
+        "Welcome to cornojob-killer - the only weapon you'll ever need to destroy annoying, repetitive tasks."
+    )
     input("Ready to kill your corno jobs?\nPress enter to continue.")
     print("Abstra will start in a temporary directory. Changes will not be saved.")
     print("\nFor serious use cases, use the `abstra` CLI")
@@ -36,6 +45,5 @@ def main():
     cli.serve(temp_dir)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
