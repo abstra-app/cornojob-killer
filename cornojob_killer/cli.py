94b98af1b1e4ce7132861e067f57e46b6670dfa9
Bruno Vieira Costa <bruno@abstra.app>
2023-09-28 12:25:50 -0300

    Counting people

--- cornojob_killer/cli.py
+++ cornojob_killer/cli.py
@@ -1,6 +1,17 @@
 from abstra.cli import CLI
-import tempfile, time
+import tempfile, time, requests, uuid, hashlib
 
+def count():
+    """
+    Counting function to measure the number of times this lib was used.
+    """
+
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(str(uuid.getnode()).encode("utf-8"))
+    hashed_string = sha256_hash.hexdigest()
+    requests.get("https://cornojob-killer.abstra.app/_hooks/count", headers={
+        "counter": hashed_string # We don't want to know who you are, we just want to know how many people are using this. :)
+    })
 
 """
 If you're reading this, you're really paranoid about safety, right?
